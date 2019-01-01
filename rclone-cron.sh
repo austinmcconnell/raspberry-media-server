5490c9eab41c60134b143fc8e90f7f045e183450
Austin <austin.s.mcconnell@gmail.com>
2019-01-01 14:17:25 -0600

    Add bash script to schedule rclone backups of application config

---
+++ rclone-cron.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if pidof -o %PPID -x “rclone-cron.sh”; then
+exit 1
+fi
+rclone copy /opt/appdata/plex/Library/Application\ Support/Plex\ Media\ Server/Preferences.xml remote:raspberry-pi-backup/appdata/plex/ --progress
+rclone copy /opt/appdata/nextcloud/www/nextcloud/config remote:raspberry-pi-backup/appdata/nextcloud/config --progress
+rclone copy /opt/appdata/nextcloud/www/nextcloud/themes remote:raspberry-pi-backup/appdata/nextcloud/themes --progress
+rclone copy /opt/appdata/letsencrypt remote:raspberry-pi-backup/appdata/letsencrypt --copy-links --progress
+rclone copy /opt/appdata/home-assistant remote:raspberry-pi-backup/appdata/homeassistant --exclude-from exclude-file.txt --progress
+rclone copy /opt/appdata/tautulli remote:raspberry-pi-backup/appdata/tautulli --exclude logs --progress
+exit
